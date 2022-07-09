1dc43767da0d24a570d9b3af10578a5e685e9743
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-07-09 14:44:39 -0600

    Add individual movie view and settings

--- src/App.js
+++ src/App.js
@@ -12,30 +12,37 @@ class App extends React.Component {
       data: movieData,
       currentView: "main",
       error: "",
-      selectedMovie: ""
+      selectedMovie: 0
     };
   };
 
-  handleChange(event) {
-    console.log("initial state", this.state)
-    console.log(event.target.parentElement.id)
+  handleChange = (event) => {
+    let view = this.state.currentView
+    if (view === "main") {
+      view = "individual"
+    } else {
+      view = "main"
+    };
     this.setState({
-      currentView:"individual",
+      currentView:view,
       selectedMovie:event.target.parentElement.id,
     });
-    console.log("altered state", this.state)
   };
 
   render() {
-    console.log(this.state)
+    let userSelectedMovie = this.state.data.movies.find(movie => this.state.selectedMovie == movie.id)
     return (
       <div>
         <Header />
-        <Movie />
-        <Display 
+        {this.state.currentView === "individual" && <Movie 
+        userSelectedMovieInfo={userSelectedMovie}
+        selectedMovie={this.state.selectedMovie}
+        handleChange={this.handleChange}
+        />}
+        {this.state.currentView === "main" && <Display 
           data={this.state.data}
           handleChange={this.handleChange}
-        />
+        />}
       </div>
     )
   };
